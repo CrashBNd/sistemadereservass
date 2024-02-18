5b7b676cf421933dedbf663cc87c5046fe8bc75d
Carlos Elias <eliasparra61@gmail.com>
2024-02-18 15:14:28 -0600

    f

--- src/App.js
+++ src/App.js
@@ -1,22 +1,101 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    id: '',
+    numberphone:'',
+    date: '',
+    num_table: '',
+    Number_people: '',
+    time: '',
+    validated: '',
+    ticket_id: '',
+    token_id: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Formulario enviado:', formData);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Formulario de Reserva</h1>
+        <form onSubmit={handleSubmit} className="form-container">
+          <div>
+            <label htmlFor="numberphone">Telefono :</label>
+            <input
+              type="text"
+              id="numberphone"
+              name="numberphone"
+              value={formData.numberphone}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="date">Fecha:</label>
+            <input
+              type="date"
+              id="date"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="num_table">Número de Mesa:</label>
+            <select
+              id="num_table"
+              name="num_table"
+              value={formData.num_table}
+              onChange={handleChange}
+            >
+              <option value="">Seleccionar mesa</option>
+              {[...Array(15)].map((_, index) => (
+                <option key={index} value={index + 1}>{index + 1}</option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="Number_people">Número de Personas:</label>
+            <input
+              type="text"
+              id="Number_people"
+              name="Number_people"
+              value={formData.Number_people}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="time">Hora:</label>
+            <input
+              type="time"
+              id="time"
+              name="time"
+              value={formData.time}
+              onChange={handleChange}
+            />
+          </div>
+        </form>
+        <div className="button-container">
+          <button className="button-60" role="button">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span className="text">Enviar</span>
+          </button>
+        </div>
       </header>
     </div>
   );
